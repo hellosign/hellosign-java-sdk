beb339c26445ef31ec58bb4240f554a4e1e8eca4
Chris Paul <chris@hellosign.com>
2015-02-06 10:08:18 -0800

    Add signature_request_invalid, template_created, and template_error event enum values.

--- src/main/java/com/hellosign/sdk/resource/support/types/EventType.java
+++ src/main/java/com/hellosign/sdk/resource/support/types/EventType.java
@@ -12,8 +12,8 @@ package com.hellosign.sdk.resource.support.types;
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -25,5 +25,13 @@ package com.hellosign.sdk.resource.support.types;
  */
 
 public enum EventType {
-	signature_request_viewed, signature_request_signed, signature_request_sent, signature_request_all_signed, file_error, test
+	signature_request_viewed,
+	signature_request_signed,
+	signature_request_sent,
+	signature_request_all_signed,
+	signature_request_invalid,
+	template_created,
+	template_error,
+	file_error,
+	test
 }
