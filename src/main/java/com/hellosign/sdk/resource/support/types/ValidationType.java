1bf7d494a9deb5ff6e49e4c19907c072951570d5
Chris Paul <chris@hellosign.com>
2016-05-16 09:36:11 -0700

    Add FormField validation types. Closes #4

---
+++ src/main/java/com/hellosign/sdk/resource/support/types/ValidationType.java
@@ -0,0 +1,13 @@
+package com.hellosign.sdk.resource.support.types;
+
+public enum ValidationType {
+	numbers_only,
+	letters_only,
+	phone_number,
+	bank_routing_number,
+	bank_account_number,
+	email_address,
+	zip_code,
+	social_security_number,
+	employer_identification_number
+}
