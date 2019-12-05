0a30c3aaa794083b5c1fee3955cc851e24181f88
John Spaetzel <john.spaetzel@hellosign.com>
2019-12-04 19:03:32 -0800

    Remove duplicate License messages, see LICENSE file for project

--- src/main/java/com/hellosign/sdk/resource/support/types/ValidationType.java
+++ src/main/java/com/hellosign/sdk/resource/support/types/ValidationType.java
@@ -1,26 +1,5 @@
 package com.hellosign.sdk.resource.support.types;
 
-/**
- * The MIT License (MIT)
- *
- * Copyright (C) 2017 hellosign.com
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 public enum ValidationType {
     numbers_only, letters_only, phone_number, bank_routing_number, bank_account_number, email_address, zip_code, social_security_number, employer_identification_number
 }
