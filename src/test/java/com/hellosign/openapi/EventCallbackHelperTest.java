a916e7d7b17ed7befedf93fcf5a81a37a997de59
Juan Treminio <50673996+jtreminio-dropbox@users.noreply.github.com>
2022-12-16 11:13:12 -0600

    H22SP24 release (#221)

--- src/test/java/com/hellosign/openapi/EventCallbackHelperTest.java
+++ src/test/java/com/hellosign/openapi/EventCallbackHelperTest.java
@@ -2,11 +2,12 @@ package com.hellosign.openapi;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hellosign.openapi.model.EventCallbackApiAppRequestPayload;
+import com.hellosign.openapi.model.EventCallbackRequest;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class EventCallbackHelperTest {
     public static final String APIKEY = "324e3b0840f065eb51f3fd63231d0d33daa35d4ed10d27718839e81737065782";
@@ -14,11 +15,23 @@ public class EventCallbackHelperTest {
     @Test
     public void testIsValid() throws Exception {
         ObjectMapper mapper = JSON.getDefault().getMapper();
-        JsonNode content = mapper.readTree(new FileInputStream("test_fixtures/EventCallbackHelper.json"));
-        for(JsonNode node : content) {
-            EventCallbackApiAppRequestPayload payload = mapper.convertValue(node, EventCallbackApiAppRequestPayload.class);
-            Assert.assertTrue(EventCallbackHelper.isValid(APIKEY, payload.getEvent()));
-            Assert.assertFalse(EventCallbackHelper.isValid(new StringBuilder(APIKEY).reverse().toString(), payload.getEvent()));
+        JsonNode content = mapper.readTree(
+            Files.newInputStream(Paths.get("test_fixtures/EventCallbackHelper.json"))
+        );
+
+        for (JsonNode node : content) {
+            EventCallbackRequest callbackEvent = mapper.convertValue(
+                node,
+                EventCallbackRequest.class
+            );
+
+            Assert.assertTrue(EventCallbackHelper.isValid(APIKEY, callbackEvent));
+            Assert.assertFalse(
+                EventCallbackHelper.isValid(
+                    new StringBuilder(APIKEY).reverse().toString(),
+                    callbackEvent
+                )
+            );
         }
     }
 }
